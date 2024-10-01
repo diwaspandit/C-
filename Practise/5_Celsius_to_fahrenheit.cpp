407100978b522a25b679ea8d566d5af9f358cef0
Diwas <diwas@Diwass-MacBook-Air.local>
2024-09-30 21:58:40 -0500

    deleted books, and added practise files

---
+++ Practise/5_Celsius_to_fahrenheit.cpp
@@ -0,0 +1,31 @@
+//Diwas Pandit
+//zxh15
+//September 30, 2024
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    double celsiusValue;
+    double fahrenheitValue;
+    char userChar;
+
+    celsiusValue = 0.0;
+    userChar = 'y';
+
+    while (userChar == 'y') {
+        fahrenheitValue = (celsiusValue * 9.0 / 5.0) + 32.0;
+
+        cout << celsiusValue << " C is ";
+        cout << fahrenheitValue << " F" << endl;
+
+        cout << "Type y to continue, any other to quit: ";
+        cin >> userChar;
+
+        celsiusValue = celsiusValue + 5;
+        cout << endl;
+
+    }
+
+    cout << "Goodbye!" << endl;
+}
