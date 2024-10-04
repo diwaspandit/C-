15caabaae8ed9b118da6e896eb62ae6ddd5e9573
Diwas <diwas@Diwass-MacBook-Air.local>
2024-10-04 15:32:28 -0500

    updated DSA

--- DSA/1_Patterns.cpp
+++ DSA/1_Patterns.cpp
@@ -37,10 +37,92 @@ using namespace std;
 
 //Program: Print a Triangle Pattern
 
-// Time: 47min
+// int main(){
+//     int n=5;
+     
+//     for (int i=0; i<n; i++){
+//         for (int j=1; j<=i+1; j++){
+//             cout << j << " ";
+//         }
+//         cout << endl;
+//     }
+
+// }
+
+//Program: Print a Triangle Pattern
+
+// int main(){
+//     int n=4;
+   
+//     for (int i=0; i<n; i++){
+//         char ch='A' + i;
+//         for (int j=i+1; j>0; j--){
+//             cout << ch << " ";
+//             ch -=1;
+//         }
+//         cout << endl;
+//     }
+// }
+
+
+//Program: Print Floyd's Triangle Pattern
+
+// int main(){
+
+//     int n=4;
+//     int num=1;
+//     for (int i=0; i<n; i++){
+        
+//         for (int j=0; j< i+1;j++){
+//             cout << num << " ";
+//             num +=1;
+//         }
+//         cout << endl;
+//     }
+// }
 
 
 
+//Program: Print Inverted Triangle Pattern
+
+// int main(){
+//     int n=4;
+
+//     for (int i=0; i<n; i++){
+//         char ch = 'A' + i;
+//         for (int j=0; j<i; j++){
+//             cout << " ";
+//         }
+//         for (int j=0; j<n-i; j++){
+//             cout << ch;
+//         }
+//         cout << endl;
+//     }
+
+//     return 0;
+
+// }
 
 
+//Program : Pyramid Pattern
 
+// int main(){
+//     int n=4;
+
+//     for (int i=0; i<n; i++){
+//         for (int j=0; j < n-i-1; j++){
+//             cout << " ";
+//         }
+//         for (int j=1; j < i+1; j++){
+//             cout << j ;
+//         }
+//         for (int j=i; j>1; j--){
+//             cout << j ;
+//         }
+        
+//         cout << endl;
+//     }
+// }
+
+
+// Time : 1hr 11min
