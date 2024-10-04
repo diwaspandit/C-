998c9a2a64c7324fcaa4500ee3637bc6c8cb8d21
Diwas <diwas@Diwass-MacBook-Air.local>
2024-10-03 19:06:59 -0500

    Updated DSA

---
+++ DSA/1_Patterns.cpp
@@ -0,0 +1,46 @@
+//Diwas Pandit
+//zxh15
+
+#include <iostream>
+using namespace std;
+
+//Program: Print a square pattern of stars
+//declare main function
+
+// int main (){
+//     int n=3;
+//     char start = 'A';
+//     for (int i=0; i<n; i++){ 
+//         for (int j=0; j<n; j++){
+//            cout << start << " ";
+//            start += 1;
+//         }
+//         cout << endl;
+//     }
+// }
+
+
+
+//Program: Print a Triangle Pattern
+
+// int main(){
+//     int n = 5;
+//     char start = 'A';
+//     for (int i=0; i<n; i++){
+//         for (int j=0; j< i+1; j++){
+//             cout << start << " ";
+//         }
+//         start +=1;
+//         cout << endl;
+//     }
+// }
+
+//Program: Print a Triangle Pattern
+
+// Time: 47min
+
+
+
+
+
+
